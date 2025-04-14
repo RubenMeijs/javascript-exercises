e6e2fbd2878d03c50c5d4df18960588881bc0e4d
Ruben Meijs <rubenmeijs@gmail.com>
2025-04-14 15:27:40 +0200

    Exercise 4 remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...elementsToRemove) {
+  let indicesToKeep = Array(inputArray.length).fill(true);
+
+  for (let element of elementsToRemove) {
+    for (let index = 0; index < inputArray.length; index++) {
+      if (inputArray[index] === element) {
+        indicesToKeep[index] = false;
+      }
+    }
+  }
+
+  let outputArray = [];
+
+  for (let index = 0; index < indicesToKeep.length; index++) {
+    if (indicesToKeep[index]){
+        outputArray.push(inputArray[index])
+    }
+  }
+  console.log(outputArray)
+  return outputArray;
 };
 
 // Do not edit below this line
