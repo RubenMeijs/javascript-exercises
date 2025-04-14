c47a397c47b2bc19d071a50d476987b03b4fdc48
Ruben Meijs <rubenmeijs@gmail.com>
2025-04-14 21:09:08 +0200

    Exercise 5 sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (firstInteger, secondInteger) {
+  if (
+    Number.isInteger(firstInteger) &
+    Number.isInteger(secondInteger) &
+    (firstInteger >= 0) &
+    (secondInteger >= 0)
+  ) {
+    let startValue = Math.min(firstInteger, secondInteger);
+    let endValue = Math.max(firstInteger, secondInteger);
 
+    let sumOfAllValues = 0;
+    for (let number = startValue; number <= endValue; number++) {
+      sumOfAllValues += number;
+    }
+    return sumOfAllValues;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
