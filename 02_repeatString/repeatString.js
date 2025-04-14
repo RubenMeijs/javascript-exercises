b107597db53f640c913c3f9d1c31bcf120d84de2
Ruben Meijs <rubenmeijs@gmail.com>
2025-04-14 14:48:50 +0200

    Exercise 2 repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (stringToRepeat, numberOfRepetitions) {
+  let outputString = "";
 
+  switch (numberOfRepetitions >= 0) {
+    case true:
+      for (let i = 0; i < numberOfRepetitions; i++) {
+        outputString = outputString.concat(stringToRepeat);
+      }
+      return outputString;
+    case false:
+        return "ERROR"
+  }
 };
 
+repeatString("test", 3);
 // Do not edit below this line
 module.exports = repeatString;
