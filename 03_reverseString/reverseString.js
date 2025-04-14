1680147c3263e87b62769adfe982974c8b0e435a
Ruben Meijs <rubenmeijs@gmail.com>
2025-04-14 14:57:12 +0200

    Exercise 3 reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let reversedString = ""
 
+    let stringLength =  stringToReverse.length
+
+    for (let i = 0; i < stringLength; i++) {
+        reversedString += stringToReverse.slice(-1)
+        stringToReverse = stringToReverse.slice(0,-1)
+        
+    }
+
+    return reversedString
 };
 
 // Do not edit below this line
